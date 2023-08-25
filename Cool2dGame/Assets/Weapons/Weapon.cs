bc2fa63901b271cca5b11a870707f41ef538fe45
Capyblapy <owen@skybridgeatx.com>
2023-08-25 14:37:50 -0500

    icon wow

--- Cool2dGame/Assets/Weapons/Weapon.cs
+++ Cool2dGame/Assets/Weapons/Weapon.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public enum FireTypes {SemiAutomatic, Automatic, BoltAction}
-public enum AmmoTypes {Normal }
+public enum FireTypes { SemiAutomatic, Automatic, BoltAction}
+public enum AmmoTypes { Normal, Freezing }
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Weapon", order = 1)]
 public class Weapon : ScriptableObject
 {
     [Header("General Info")]
     public string Name;
+    public Image Icon;
     public FireTypes FireType;
     public AmmoTypes AmmoType;
 
