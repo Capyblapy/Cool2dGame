0ae0435b28f97a5980666f44058fbf832e271c46
MaxKennedy08 <max.k@skybridgeatx.com>
2023-10-04 15:21:16 -0500

    i made placeholder sprites for the walls and floor, and made progress on room generation

--- Cool2dGame/Assets/Rooms/TileScript.cs
+++ Cool2dGame/Assets/Rooms/TileScript.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public enum tileTypes {floor, wall, hazard, chest};
 
+
 public class TileScript : MonoBehaviour
 {
     public tileTypes tileType;
 
-    
-    
+    public SpriteRenderer tileRend;
+    public BoxCollider2D tileCol;
+
+    public Sprite[] tileSprites;
 
     public void setTile(tileTypes newTileType)
     {
@@ -18,8 +21,12 @@ public class TileScript : MonoBehaviour
         switch (tileType)
         {
             case tileTypes.floor:
+                tileCol.enabled = false;
+                tileRend.sprite = tileSprites[0];
                 break;
             case tileTypes.wall:
+                tileCol.enabled = true;
+                tileRend.sprite = tileSprites[1];
                 break;
             case tileTypes.hazard:
                 break;
