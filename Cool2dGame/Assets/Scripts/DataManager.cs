f7888fd359bd7ae50f219f34f2ed89cc06c62e5e
Capyblapy <owen@skybridgeatx.com>
2023-10-30 10:25:31 -0500

    did data stuff idk

--- Cool2dGame/Assets/Scripts/DataManager.cs
+++ Cool2dGame/Assets/Scripts/DataManager.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public SaveData LoadGameData()
     {
-        
+        string filePath = Application.persistentDataPath + "/not.data";
+        //print(filePath);
+        if (File.Exists(filePath))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+
+            SaveData data = formatter.Deserialize(stream) as SaveData;
+            print("Loaded data from " + filePath);
+
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogWarning("Save file not found in " + filePath + ", creating new save data");
+
+            //Money = Money;
+
+            SaveData data = new SaveData(gameObject.GetComponent<InventoryManager>());
+            SaveGameData();
+
+            return data;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveGameData()
     {
-        
+        BinaryFormatter formatter = new BinaryFormatter();
+        string filePath = Application.persistentDataPath + "/not.data";
+        FileStream stream = new FileStream(filePath, FileMode.Create);
+
+        SaveData data = new SaveData(gameObject.GetComponent<InventoryManager>());
+        print("Saved data at " + filePath);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+}
+
+[System.Serializable]
+public class SaveData
+{
+    public List<GameObject> Inventory;
+
+    public SaveData(InventoryManager im)
+    {
+        Inventory = im.Inventory;
     }
 }
